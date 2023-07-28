988c9d0cb326f4b484002461736429d3a48bb335
mostafa farrag <adam.mostafa@outlook.de>
2023-07-28 22:43:03 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-    res.send("Express on Vercel");
+    res.send("Express on Vercel 3");
 });
 app.listen(5000, () => {
     console.log("Running on port 5000.");
